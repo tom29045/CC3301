c444c541fc5e6b0085ae98813898bf757c275cd8
tom29045 <tomascanales8deabril@gmail.com>
2026-08-18 12:12:30 -0400

    Comprimir function complete

--- Tarea1/comprimir.c
+++ Tarea1/comprimir.c
@@ -6,5 +6,16 @@
 #include "comprimir.h"
 
 uint comprimir(uint a[], int nbits) {
-  ...
+  uint r = 0;
+  int i = 0;
+  int espacio = sizeof(uint) << 3;
+  while (espacio >= nbits) {
+    uint movimiento = (1 << (nbits - 1) << 1) - 1;
+    uint trozo = a[i] & (movimiento);
+    r <<= nbits;
+    r |= trozo;
+    i += 1;
+    espacio -= nbits;
+  }
+  return r;
 }
